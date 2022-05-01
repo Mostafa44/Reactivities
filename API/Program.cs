3fb194399ee8b6b272c37571c8acb5d9c6c84865
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-05-01 20:11:44 +0200

    creating the database

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +16,29 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            //the above line is that once we are done with this scope 
+            //the scope would be disposed 
+            var services = scope.ServiceProvider;
+            try
+            {
+                //we are using the service locator pattern 
+                //so that we can get the DataConetext and use it in our local variable
+
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+
+            }
+            catch (System.Exception ex)
+            {
+
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during Migration");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
