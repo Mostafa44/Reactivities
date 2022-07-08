f5b6e075983cc14b20616db67d36409c9c54c912
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-07-08 18:03:01 +0200

    creating the Query handler

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,6 +43,7 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
 
         }
 
