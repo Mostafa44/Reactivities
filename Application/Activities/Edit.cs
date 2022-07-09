fdbee23c82e6b545c63ea97696092a4e1787f98a
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-07-09 10:32:39 +0200

    Add Automapper

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -19,16 +20,18 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _conetxt;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext conetxt)
+            public Handler(DataContext conetxt, IMapper mapper)
             {
                 _conetxt = conetxt;
+                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _conetxt.Activities.FindAsync(request.Activity.Id);
-                activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
                 await _conetxt.SaveChangesAsync();
                 return Unit.Value;
 
