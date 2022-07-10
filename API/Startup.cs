07cb3a0ad3a7927748d14478b4013d755122a5ac
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-07-10 22:39:35 +0200

    Add the Cors Configuation resolve a typo

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000")
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
 
             });
